177026583eab68101d1bee4aedbfa58a6dc56d0d
Clive Murray <clive.murray@digital.justice.gov.uk>
2015-01-13 16:42:30 +0000

    deploy status widget
    DRY job, remove old CSS, html to slim for consistency

--- config/settings.rb
+++ config/settings.rb
@@ -10,6 +10,7 @@ include ActionView::Helpers::DateHelper
 Dotenv.load
 
 JENKINS_URI = URI.parse(ENV['JENKINS_URL'])
+JENKINS_INTERNAL_URI = URI.parse(ENV['JENKINS_INTERNAL_URL'])
 JENKINS_AUTH = {
   'name' => ENV['JENKINS_USER'],
   'password' => ENV['JENKINS_TOKEN']
