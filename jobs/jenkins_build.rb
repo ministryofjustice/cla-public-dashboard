a56146b605843275a4bc0e5961cf1f182f9ce8cd
Tyom Semonov <inspiro@gmail.com>
2015-01-16 11:37:38 +0000

    Update styles for PR integration builds

--- jobs/jenkins_build.rb
+++ jobs/jenkins_build.rb
@@ -6,7 +6,7 @@ include ActionView::Helpers::DateHelper
 
 # the key of this mapping must be a unique identifier for your job, the according value must be the name that is specified in jenkins
 job_mapping = {
-  'JOB' => { :job => 'CLA Public - Integration PRs', :pre_job => nil}
+  'JOB' => { :job => 'CLA Public - Integration PRs' }
 }
 
 def get_number_of_failing_tests(job_name)
@@ -18,9 +18,9 @@ def get_completion_percentage(job_name)
   build_info = get_json_for_job(job_name)
   prev_build_info = get_json_for_job(job_name, 'lastCompletedBuild')
 
-  return 0 if not build_info["building"]
-  last_duration = (prev_build_info["duration"] / 1000).round(2)
-  current_duration = (Time.now.to_f - build_info["timestamp"] / 1000).round(2)
+  return 0 if not build_info['building']
+  last_duration = (prev_build_info['duration'] / 1000).round(2)
+  current_duration = (Time.now.to_f - build_info['timestamp'] / 1000).round(2)
   return 99 if current_duration >= last_duration
   ((current_duration * 100) / last_duration).round(0)
 end
@@ -38,17 +38,16 @@ end
 
 job_mapping.each do |title, jenkins_project|
   current_status = nil
+
   SCHEDULER.every '3s', :first_in => 0 do |job|
     last_status = current_status
     build_info = get_json_for_job(jenkins_project[:job])
     current_status = build_info["result"]
-    if build_info["building"]
-      current_status = "BUILDING"
+    percent = 0
+
+    if build_info['building']
+      current_status = 'BUILDING'
       percent = get_completion_percentage(jenkins_project[:job])
-    # elsif jenkins_project[:pre_job]
-    #   pre_build_info = get_json_for_job(jenkins_project[:pre_job])
-    #   current_status = "PREBUILD" if pre_build_info["building"]
-    #   percent = get_completion_percentage(jenkins_project[:pre_job])
     end
 
     parameters = build_info['actions'][0]['parameters']
@@ -59,13 +58,16 @@ job_mapping.each do |title, jenkins_project|
     end
 
     send_event(jenkins_project[:job], {
-      title: jenkins_project[:job].sub('CLA Public - ', ''),
+      title: 'Pull Request build',
       currentResult: current_status,
       lastResult: last_status,
-      value: percent,
-      pull_title: pull_title,
+      percent: percent,
+      rotaryValue: percent * 240 / 100 - 120,
+      pullTitle: pull_title,
       commit_author: commit_author,
-      is_building: current_status == 'BUILDING',
+      isBuilding: current_status == 'BUILDING',
+      isSuccess: current_status == 'SUCCESS',
+      isFailure: current_status == 'FAILURE',
       lastBuiltAgo: time_ago_in_words(Time.at(build_info["timestamp"]/1000))
     })
   end
