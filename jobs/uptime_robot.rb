405cf6a4729f1470104a9caeb709e1c483c9857d
Tyom Semonov <inspiro@gmail.com>
2015-01-22 18:04:52 +0000

    Simple uptime widget using uptimerobot.com

---
+++ jobs/uptime_robot.rb
@@ -0,0 +1,27 @@
+require 'uptimerobot'
+
+STATUSES = {
+  '0' => 'paused',
+  '1' => 'not checked yet',
+  '2' => 'up',
+  '8' => 'seems down',
+  '9' => 'down'
+}
+TYPES = {
+  '1' => 'HTTP(s)',
+  '2' => 'Keyword',
+  '3' => 'Ping',
+  '4' => 'Port',
+}
+
+client = UptimeRobot::Client.new(apiKey: 'u200741-b3910f5b96a269d52fe51b73')
+
+SCHEDULER.every '5m', first_in: 0 do
+  monitors = []
+
+  client.getMonitors['monitors']['monitor'].each do |m|
+    monitors << { type: TYPES[m['type']], status: STATUSES[m['status']] }
+  end
+
+  send_event('uptime_robot', { monitors: monitors })
+end
---
+++ widgets/uptime_robot/uptime_robot.coffee
@@ -0,0 +1,3 @@
+class Dashing.UptimeRobot extends Dashing.Widget
+  onData: (data) ->
+    console.log(data)
