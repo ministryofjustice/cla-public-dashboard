f55cd383d56fafa22da99645502f5b0195e4000a
Tyom Semonov <inspiro@gmail.com>
2015-09-14 14:16:51 +0100

    Use GA outcome goals and add change indicator from the day before

--- jobs/ga_stats.rb
+++ jobs/ga_stats.rb
@@ -1,8 +1,8 @@
 require 'google/api_client'
 require 'date'
 
-START_DATE = "yesterday"
-END_DATE = "yesterday"
+START_DATE = (Time.now - (24*60*60) * 2).strftime( "%Y-%m-%d" ) # day before yesterday
+END_DATE = (Time.now - (24*60*60)).strftime( "%Y-%m-%d" ) # yesterday
 
 # Get the Google API client
 client = Google::APIClient.new(
@@ -10,7 +10,36 @@ client = Google::APIClient.new(
   application_version: '0.01'
 )
 
-visitors = []
+def add_to_batch(batch, api, metric)
+  batch.add({
+    api_method: api.data.ga.get,
+    parameters: {
+      'ids' => "ga:#{GA_VIEW_ID}",
+      'start-date' => START_DATE,
+      'end-date' => END_DATE,
+      'metrics' => "ga:#{metric}",
+      'dimensions' => 'ga:date'
+    }
+  }) do |result|
+    yesterday = result.data.rows.last.last.to_f.round(2)
+    day_before = result.data.rows.first.last.to_f.round(2)
+    change = 'same'
+
+    ratio = yesterday / day_before
+
+    if ratio > 1
+      change = 'up'
+    elsif ratio < 1
+      change = 'down'
+    end
+
+    yield({
+      value: yesterday,
+      prev: day_before,
+      change: change
+    })
+  end
+end
 
 # Load your credentials for the service account
 key = Google::APIClient::KeyUtils.load_from_pkcs12(GA_KEY_FILE, 'notasecret')
@@ -27,100 +56,28 @@ SCHEDULER.every '3h', first_in: 0 do
   analytics = client.discovered_api('analytics','v3')
   batch = Google::APIClient::BatchRequest.new
 
-  users = 0
-  page_views = 0
-  unique_page_views = 0
-  eligible_users = 0
-  ineligible_users = 0
-  face_to_face_users = 0
-  contact_users = 0
+  result = {}
 
-  base = {
-    api_method: analytics.data.ga.get,
-    parameters: {
-      'ids' => "ga:#{GA_VIEW_ID}",
-      'start-date' => START_DATE,
-      'end-date' => END_DATE,
-      'output' => 'dataTable',
-      'metrics' => "ga:users, ga:pageviews, ga:uniquePageviews"
-    }
-  }
-
-  batch.add(base) do |result|
-    users = result.data.totalsForAllResults['ga:users'].to_i
-    page_views = result.data.totalsForAllResults['ga:pageviews'].to_i
-    unique_page_views = result.data.totalsForAllResults['ga:uniquePageviews'].to_i
-  end
-
-  eligible = {
-    api_method: analytics.data.ga.get,
-    parameters: {
-      'ids' => "ga:#{GA_VIEW_ID}",
-      'start-date' => START_DATE,
-      'end-date' => END_DATE,
-      'metrics' => "ga:users",
-      'filters' => "ga:pagePath=@eligible"
-    }
-  }
-
-  batch.add(eligible) do |result|
-    eligible_users = result.data.totalsForAllResults['ga:users'].to_i
-  end
-
-  ineligible = {
-    api_method: analytics.data.ga.get,
-    parameters: {
-      'ids' => "ga:#{GA_VIEW_ID}",
-      'start-date' => START_DATE,
-      'end-date' => END_DATE,
-      'metrics' => "ga:users",
-      'filters' => "ga:pagePath=@help-organisations"
-    }
-  }
-
-  batch.add(ineligible) do |result|
-    ineligible_users = result.data.totalsForAllResults['ga:users'].to_i
-  end
-
-  face_to_face = {
-    api_method: analytics.data.ga.get,
-    parameters: {
-      'ids' => "ga:#{GA_VIEW_ID}",
-      'start-date' => START_DATE,
-      'end-date' => END_DATE,
-      'metrics' => "ga:users",
-      'filters' => "ga:pagePath=@face-to-face"
-    }
-  }
-
-  batch.add(face_to_face) do |result|
-    face_to_face_users = result.data.totalsForAllResults['ga:users'].to_i
-  end
-
-  contact = {
-    api_method: analytics.data.ga.get,
-    parameters: {
-      'ids' => "ga:#{GA_VIEW_ID}",
-      'start-date' => START_DATE,
-      'end-date' => END_DATE,
-      'metrics' => "ga:users",
-      'filters' => "ga:pagePath=@contact"
-    }
-  }
-
-  batch.add(contact) do |result|
-    contact_users = result.data.totalsForAllResults['ga:users'].to_i
-  end
+  # Sessions
+  add_to_batch(batch, analytics, 'sessions') {|r| result[:sessions] = r}
+  # Eligible rate
+  add_to_batch(batch, analytics, 'goal8ConversionRate') {|r| result[:eligible] = r}
+  # Incomplete (skip to contact)
+  add_to_batch(batch, analytics, 'goal6ConversionRate') {|r| result[:incomplete] = r}
+  # Ineligible (Scope)
+  add_to_batch(batch, analytics, 'goal1ConversionRate') {|r| result[:ineligible_scope] = r}
+  # Ineligible (Means)
+  add_to_batch(batch, analytics, 'goal2ConversionRate') {|r| result[:ineligible_means] = r}
+  # Family mediation
+  add_to_batch(batch, analytics, 'goal5ConversionRate') {|r| result[:family_mediation] = r}
+  # Provisional (need more info)
+  add_to_batch(batch, analytics, 'goal7ConversionRate') {|r| result[:provisional] = r}
+  # Face-to-face (Scope)
+  add_to_batch(batch, analytics, 'goal3ConversionRate') {|r| result[:f2f_scope] = r}
+  # Face-to-face (Means)
+  add_to_batch(batch, analytics, 'goal4ConversionRate') {|r| result[:f2f_means] = r}
 
   client.execute(batch)
 
-  send_event('ga_stats', {
-    users: users,
-    page_views: page_views,
-    unique_page_views: unique_page_views,
-    eligible_users: eligible_users,
-    ineligible_users: ineligible_users,
-    face_to_face_users: face_to_face_users,
-    contact_users: contact_users
-  })
+  send_event('ga_stats', result)
 end
