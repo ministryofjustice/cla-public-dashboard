7261743ae67e8e0cc7d67428f5caab3638cbfe8d
Josh Rowe <josh@s-block.com>
2016-03-24 16:24:31 +0000

    move staging and prod deploys to new aws jenkins

--- jobs/deploys.rb
+++ jobs/deploys.rb
@@ -4,25 +4,34 @@ require 'json'
 JOBS = [
   { label: 'Develop',     name: 'cla_public-DEVELOP', uri: JENKINS_URI },
   { label: 'Master',      name: 'cla_public-MASTER', uri: JENKINS_URI },
-  { label: 'Staging',     name: 'CLA Deploy PUBLIC to STAGING', uri: JENKINS_INTERNAL_URI },
-  { label: 'Production',  name: 'CLA Deploy PUBLIC to PRODUCTION', uri: JENKINS_INTERNAL_URI }
 ]
+
+DEPLOYS = [
+  { label: 'Staging',     name: 'cla_public-DEPLOY', uri: JENKINS_URI, env: 'staging' },
+  { label: 'Production',  name: 'cla_public-DEPLOY', uri: JENKINS_URI, env: 'prod' }
+]
+
 REQUEST_STR = "/api/json?tree=jobs[name,color,number,lastBuild[number,timestamp],healthReport[*]]"
+DEPLOY_REQUEST_STRING = "/api/json?tree=builds[number,timestamp,result,description,actions[parameters[name,value]]]"
 
 def get_jobs(jobs)
   grouped_jobs = jobs.group_by { |j| j[:uri] }
   results = []
   jenkins_jobs = []
 
-  grouped_jobs.each do |uri, group_jobs|
+  def get_request(uri, request_str)
     http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(REQUEST_STR)
+    request = Net::HTTP::Get.new(request_str)
 
     if JENKINS_AUTH['name']
       request.basic_auth(JENKINS_AUTH['name'], JENKINS_AUTH['password'])
     end
 
-    response = http.request(request)
+    http.request(request)
+  end
+
+  grouped_jobs.each do |uri, group_jobs|
+    response = get_request(uri, REQUEST_STR)
 
     jenkins_jobs << JSON.parse(response.body)['jobs'].select do |job|
       jobs.map { |j| j[:name] }.include?(job['name'])
@@ -48,6 +57,19 @@ def get_jobs(jobs)
     end
 
     result['status'] = status
+    results << result
+  end
+
+  deploy_response = get_request(JENKINS_URI, '/job/cla_public-DEPLOY' + DEPLOY_REQUEST_STRING)
+  deploys = JSON.parse(deploy_response.body)['builds']
+
+  DEPLOYS.each do |deploy|
+    dr = deploys.select { |d| d['actions'].first['parameters'][1]['value'] == deploy[:env] }.first
+    result = {}
+    result['label'] = deploy[:label]
+    result['status'] = { type: dr['result'].to_s.downcase, icon: dr['result'].to_s.downcase }
+    result['lastBuild'] = {}
+    result['lastBuild']['timestamp'] = dr['timestamp']
     results << result
   end
 
